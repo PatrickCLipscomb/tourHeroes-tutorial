2c38e92080ada9fd3c09bd34e95be3eeb6db2fbe
Kyle Helyar and patrick lipscomb <kyle@>
2016-08-11 10:20:49 -0700

    will add a routing file and some structural changes.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,29 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { AppComponent }   from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService }  from './hero.service';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { routing } from './app.routing';
+import { DashboardComponent }   from './dashboard.component';
+
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
